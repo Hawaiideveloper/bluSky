e8456ef0dc8c956f937da3d5fc462f9e84f44927
Corey Albright <coreymalbright@gmail.com>
2020-08-24 23:51:42 -0400

    fixed the reference point to point to index.html for response redirect

--- register_models/app.js
+++ register_models/app.js
@@ -44,7 +44,7 @@ app.get('/',function(req,res){
 res.set({ 
 	'Access-control-Allow-Origin': '*'
 	}); 
-return res.redirect('index-form.html'); 
+return res.redirect('index.html'); 
 }).listen(3000) 
 
 
