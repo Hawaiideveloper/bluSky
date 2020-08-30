b094855a60f97cc6eea41f943c559b4046c59547
Corey Albright <coreymalbright@gmail.com>
2020-08-29 22:05:59 -0400

    update form registration

---
+++ non-paid-members/run_it.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+npm init
+
+
+npm install express --save
+
+node app.js
+
+echo " please checkout http://localhost:80"
+
+# https://www.jenniferbland.com/saving-data-to-mongodb-database-from-node-js-application-tutorial/
+
+
+# https://www.youtube.com/watch?v=rOfT83_eKWk
